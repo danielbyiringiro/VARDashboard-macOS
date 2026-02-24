82d931510f268ba8bf62753c16f9a92912b42f00
Daniel Byiringiro <daniel.byiringiro@ashesi.edu.gh>
2026-02-24 11:19:17 +0000

    Initial Commit

---
+++ VARDashboard-macOS/VARDashboard_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VARDashboard_macOSApp.swift
+//  VARDashboard-macOS
+//
+//  Created by Daniel Byiringiro on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct VARDashboard_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
